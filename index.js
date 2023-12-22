75da849425906c213343880c4a8aa2eca3322bb2
Alien Brain <murad.dub@gmail.com>
2023-12-22 14:46:23 +0600

    vercel deployed

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("SERVER is running!");
-});
-
-app.listen(port, () => {
-  console.log(`SERVER running on port: ${port}`);
-});
+app.use(cors());
+app.use(express.json());
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-console.log(process.env.DB_USER, process.env.DB_PASS);
+// mongodb connection
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// console.log(process.env.DB_USER, process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m38robg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -27,12 +23,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+// API to load all tasks
+const tasksCollection = client.db("todoTaskDB").collection("allTasks");
+
+app.get("/allTasks", async (req, res) => {
+  console.log(req.query.email);
+
+  let query = {};
+
+  if (req.query?.email) {
+    query = { addedByUser: req.query.email };
+  }
+  const result = await tasksCollection.find(query).toArray();
+  res.send(result);
+});
+
+// API to create a task
+app.post("/allTasks", async (req, res) => {
+  console.log("Request Body:", req.body);
+  const item = req.body;
+  console.log("From Client:", item);
+  const result = await tasksCollection.insertOne(item);
+  res.send(result);
+});
+
+// connect to server
+app.get("/", (req, res) => {
+  res.send("SERVER is running!");
+});
+
+app.listen(port, () => {
+  console.log(`SERVER running on port: ${port}`);
+});
